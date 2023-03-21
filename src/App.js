0416c935f81104911e21a31dca7f8214f0e86387
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-03-21 18:25:51 +0000

    Resolve errors

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 
 /* Pages */
 import Home from "./pages/Home/HomePage";
-import Project from "./pages/Project/ProjectPage";
-import ProjectGame from "./pages/Project/ProjectGame";
+
+
 
 import RouterScrollTop from "./components/ScrollToTop/RouterScrollTop";
 
@@ -15,8 +15,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route exact path="/project" element={<Project />}></Route>
-        <Route exact path="/project/game" element={<ProjectGame />} />
       </Routes>
     </>
   );
